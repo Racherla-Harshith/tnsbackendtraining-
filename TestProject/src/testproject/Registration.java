6b045e0c441b5690e17651fc90a17ddef4950055
Racherla-Harshith <rharshith2005@gmail.com>
2025-08-04 20:56:47 +0530

    Registration checking

---
+++ TestProject/src/testproject/Registration.java
@@ -0,0 +1,13 @@
+package testproject;
+
+public class Registration {
+	
+	public void print() {
+        System.out.println("Welcome to Registration!");
+    }
+
+    public static void main(String[] args) {
+        Registration db = new Registration();
+        db.print(); 
+    }
+}
