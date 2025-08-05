ed36cec50acdbbfa22266336072b8287de9e09d6
Racherla-Harshith <rharshith2005@gmail.com>
2025-08-05 21:45:16 +0530

    Operators done sucessfully

---
+++ TestProject/src/testproject/OperatorsDemo.java
@@ -0,0 +1,23 @@
+package testproject;
+
+public class OperatorsDemo {
+
+	public static void main(String[] args) {
+		//post increment 
+		int a = 20;
+		a = a++;  
+		System.out.println(a);
+		//pre increment
+		int b = 20;
+		b = ++b;
+		System.out.println(b);
+		//post decrement
+		int c = 20;
+		c = c--;
+		System.out.println(c);
+		//pre decrement
+		int d = 20;
+		d = --d;
+		System.out.println(d);
+	}
+} 
